3c3f504a280f87f41cc5743d46b4193add148237
shixinzhu <shixinzhu@jintengtech.com>
2023-08-15 09:38:15 +0800

    feat: :sparkles: vitest tsx

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config'
-
+import VueJsx from '@vitejs/plugin-vue-jsx'
+import Vue from '@vitejs/plugin-vue'
 export default defineConfig({
+  plugins: [VueJsx(), Vue()],
   test: {
     globals: true,
     environment: 'jsdom',
+    // @ts-expect-error
+    transformMode: {
+      web: [/\.[jt]sx$/],
+    },
   },
-});
+})

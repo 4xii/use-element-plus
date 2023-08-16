248887414b39c4ac1b26240d24f69157350faa3a
shixinzhu <shixinzhu@jintengtech.com>
2023-08-16 11:19:20 +0800

    feat: :sparkles: playground alias use ep

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,6 +3,7 @@ import Inspect from 'vite-plugin-inspect'
 import UnoCSS from 'unocss/vite'
 import Vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite';
+import path from 'path';
 export default defineConfig({
   build: {
     outDir: './dist/vite',
@@ -15,4 +16,12 @@ export default defineConfig({
       build: true,
     }),
   ],
+  resolve: {
+    alias: [
+      {
+        find: 'use-element-plus',
+        replacement: path.resolve(__dirname, '../src/index.ts'),
+      },
+    ],
+  },
 })

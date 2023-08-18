53531a47f08422b400946aaf5738cddfcba3c324
4xii <975795875@qq.com>
2023-08-18 15:01:29 +0800

    feat: :sparkles: refactor Form Component and add test cases for form initialization and form submission

--- playground/src/examples/use-form/index.vue
+++ playground/src/examples/use-form/index.vue
@@ -1,197 +1,89 @@
 <script lang="ts" setup>
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 import { useForm } from 'use-element-plus'
 import { type FormInstance, type FormRules } from 'element-plus'
 
-interface RuleForm {
-  name: string
-  region: string
-  count: string
-  date1: string
-  date2: string
-  delivery: boolean
-  type: string[]
-  resource: string
-  desc: string
+const props = defineProps({
+  onSubmit:Function
+})
+
+const formRef = ref<FormInstance>()
+
+const defaultValues = {
+  fieldName: 'initialValue',
+  nestedField: {
+    nestedFieldName: 'nestedInitialValue',
+  },
 }
 
-const options = Array.from({ length: 10000 }).map((_, idx) => ({
-  value: `${idx + 1}`,
-  label: `${idx + 1}`,
-}))
-
-const formSize = ref('default')
-
-const ruleFormRef = ref<FormInstance>()
-
-const defaultRuleFormData: RuleForm = {
-  name: 'Hello',
-  region: '',
-  count: '',
-  date1: '',
-  date2: '',
-  delivery: true,
-  type: [],
-  resource: '',
-  desc: '',
+const basicValues = {
+  fieldName: '',
+  nestedField: {
+    nestedFieldName: 'basicNestedValue',
+  },
 }
 
 const {
-  formData: ruleForm,
+  formData,
   handleSubmit,
   resetFields,
   handleValidate,
-  scrollToField
-} = useForm({ formRef: ruleFormRef, initialFormData: defaultRuleFormData })
+  // scrollToField
+} = useForm({ formRef, initialFormData: defaultValues, basicFormData: basicValues })
 
-const rules = reactive<FormRules>({
-  name: [
-    { required: true, message: 'Please input Activity name', trigger: 'blur' },
-    { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
-  ],
-  region: [
+const rules: FormRules = {
+  fieldName: [
     {
       required: true,
-      message: 'Please select Activity zone',
+      message: 'Please input fieldName',
+      trigger: 'blur',
+    },
+  ],
+  'nestedField.nestedFieldName': [
+    {
+      required: true,
+      message: 'Please input nestedFieldName',
       trigger: 'change',
     },
   ],
-  count: [
-    {
-      required: true,
-      message: 'Please select Activity count',
-      trigger: 'change',
-    },
-  ],
-  date1: [
-    {
-      type: 'date',
-      required: true,
-      message: 'Please pick a date',
-      trigger: 'change',
-    },
-  ],
-  date2: [
-    {
-      type: 'date',
-      required: true,
-      message: 'Please pick a time',
-      trigger: 'change',
-    },
-  ],
-  type: [
-    {
-      type: 'array',
-      required: true,
-      message: 'Please select at least one activity type',
-      trigger: 'change',
-    },
-  ],
-  resource: [
-    {
-      required: true,
-      message: 'Please select activity resource',
-      trigger: 'change',
-    },
-  ],
-  desc: [
-    { required: true, message: 'Please input activity form', trigger: 'blur' },
-  ],
-})
+}
 
-const submitForm = handleSubmit(async (data, validRes) => {
-  const { valid, fields } = validRes
-  if (valid) {
-    console.log('submit!')
-  } else {
-    console.log('error submit!', fields)
-  }
+const handleInitialReset = () => {
+  resetFields({ type: 'initial' })
+}
+
+const handleBasicReset = () => {
+  resetFields({ type: 'basic' })
+}
+
+const submit = handleSubmit((value, fields) => {
+  props.onSubmit && props.onSubmit(value, fields);
 })
 
 const validate = async () => {
-  const res = await handleValidate().catch((error) =>
-    console.log('e :>>', error)
-  )
-  console.log('res :>>', res)
+  await handleValidate();
 }
 </script>
 
 <template>
-  <el-form
-    ref="ruleFormRef"
-    :model="ruleForm"
-    :rules="rules"
-    label-width="120px"
-    :size="formSize"
-    status-icon
-  >
-    <el-form-item label="Activity name" prop="name">
-      <el-input v-model="ruleForm.name" />
+  <el-form ref="formRef" :model="formData" :rules="rules">
+    <el-form-item ref="fieldNameItem" prop="fieldName" label="fieldName">
+      <el-input v-model="formData.fieldName" ref="fieldNameInput" />
     </el-form-item>
-    <el-form-item label="Activity zone" prop="region">
-      <el-select v-model="ruleForm.region" placeholder="Activity zone">
-        <el-option label="Zone one" value="shanghai" />
-        <el-option label="Zone two" value="beijing" />
-      </el-select>
-    </el-form-item>
-    <el-form-item label="Activity count" prop="count">
-      <el-select-v2
-        v-model="ruleForm.count"
-        placeholder="Activity count"
-        :options="options"
-      />
-    </el-form-item>
-    <el-form-item label="Activity time" required>
-      <el-col :span="11">
-        <el-form-item prop="date1">
-          <el-date-picker
-            v-model="ruleForm.date1"
-            type="date"
-            label="Pick a date"
-            placeholder="Pick a date"
-            style="width: 100%"
-          />
-        </el-form-item>
-      </el-col>
-      <el-col class="text-center" :span="2">
-        <span class="text-gray-500">-</span>
-      </el-col>
-      <el-col :span="11">
-        <el-form-item prop="date2">
-          <el-time-picker
-            v-model="ruleForm.date2"
-            label="Pick a time"
-            placeholder="Pick a time"
-            style="width: 100%"
-          />
-        </el-form-item>
-      </el-col>
-    </el-form-item>
-    <el-form-item label="Instant delivery" prop="delivery">
-      <el-switch v-model="ruleForm.delivery" />
-    </el-form-item>
-    <el-form-item label="Activity type" prop="type">
-      <el-checkbox-group v-model="ruleForm.type">
-        <el-checkbox label="Online activities" name="type" />
-        <el-checkbox label="Promotion activities" name="type" />
-        <el-checkbox label="Offline activities" name="type" />
-        <el-checkbox label="Simple brand exposure" name="type" />
-      </el-checkbox-group>
-    </el-form-item>
-    <el-form-item label="Resources" prop="resource">
-      <el-radio-group v-model="ruleForm.resource">
-        <el-radio label="Sponsorship" />
-        <el-radio label="Venue" />
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="Activity form" prop="desc">
-      <el-input v-model="ruleForm.desc" type="textarea" />
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="submitForm()"> Create </el-button>
-      <el-button @click="resetFields()">Reset</el-button>
-      <!-- <el-button type="info" @click="clearValidate()">Clear Validate</el-button> 添加了清除校验的按钮 -->
-      <el-button type="warning" @click="validate()">Validate Form</el-button>
+    <el-form-item ref="nestedFieldNameItem" prop="nestedField.nestedFieldName" label="nestedField">
+      <el-input v-model="formData.nestedField.nestedFieldName" ref="nestedFieldNameInput" />
     </el-form-item>
   </el-form>
+  <button class="initalResetButton" @click="handleInitialReset">
+    Reset to Initial
+  </button>
+  <button class="basicResetButton" @click="handleBasicReset">
+    Reset to Basic
+  </button>
+  <button class="submitButton" @click="submit">
+    submit
+  </button>
+  <button class="validateButton" @click="validate">
+    validate
+  </button>
 </template>

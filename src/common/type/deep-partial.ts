466f5bd5f622cc63a140b5d083936f37a06339d7
4xii <975795875@qq.com>
2023-08-21 14:00:54 +0800

    feat: :sparkles: update package.json version to "0.0.11-dev" and add BrowserNativeObject types

--- src/common/type/deep-partial.ts
+++ src/common/type/deep-partial.ts
@@ -1,3 +1,5 @@
-export type DeepPartial<T> = T extends BrowserNativeObject | NestedValue
+export type BrowserNativeObject = Date | FileList | File;
+
+export type DeepPartial<T> = T extends BrowserNativeObject
   ? T
   : { [K in keyof T]?: DeepPartial<T[K]> };
